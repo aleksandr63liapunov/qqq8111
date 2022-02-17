92040c6a15e4fbe319825a67a70f6b0f7bd8b804
aleksandr63liapunov <liapunov.sania@mail.ru>
2022-02-17 14:31:27 +0400

    change3

--- src/main/java/service/Service.java
+++ src/main/java/service/Service.java
@@ -4,16 +4,17 @@ import dao.DaoInt;
 import model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+
 import java.util.List;
 @Repository
-@org.springframework.transaction.annotation.Transactional
+
 public class Service implements Serviceint {
 
     private final DaoInt daoInt ;
 @Autowired
-    public Service() {
+    public Service(DaoInt daoInt) {
         this.daoInt=daoInt;
     }
 
